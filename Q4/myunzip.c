9e69f78915713dd91bfbe37e6e09eb9c3d2981b4
Aviost111 <Aviostroff@yahoo.com>
2024-01-28 17:01:51 +0200

    .

--- Q4/myunzip.c
+++ Q4/myunzip.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <wait.h>
+#include <fcntl.h>
 
 int myunzip(char *file) {
     char tmp_template[] = "./archive.XXXXXX";
@@ -22,34 +23,65 @@ int myunzip(char *file) {
     pid_t pid = fork();
 
     if (pid == 0) {
-        execlp("gpg", "gpg", "-d", file , NULL);
+        // Child process: Execute gpg command to decrypt the file and write to the temporary file
+        dup2(fileno(tmp_file), STDOUT_FILENO);  // Redirect stdout to the temporary file
+        execlp("gpg", "gpg", "--decrypt", file, NULL);
         perror("gpg");
         exit(1);
     } else {
+        // Parent process: Wait for the child process to finish
         wait(NULL);
     }
 
+    // Rewind the temporary file pointer to the beginning
     rewind(tmp_file);
 
-    pid = fork();
+    int pipe_fd[2];
+    if (pipe(pipe_fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
 
+    pid = fork();
     if (pid == 0) {
-        execlp("gunzip", "gunzip", "-c", tmp_template, NULL);
+        // Child process: Execute gunzip command, reading from the temporary file
+        close(pipe_fd[0]);  // Close the read end of the pipe
+        dup2(fileno(tmp_file), STDIN_FILENO);  // Redirect stdin to the temporary file
+        dup2(pipe_fd[1], STDOUT_FILENO);  // Redirect stdout to the write end of the pipe
+        execlp("gunzip", "gunzip", "-c", "-", NULL);
         perror("gunzip");
         exit(1);
     } else {
+        // Parent process: Wait for the child process to finish
         wait(NULL);
+        close(pipe_fd[1]);  // Close the write end of the pipe
+
+        // Rewind the temporary file pointer to the beginning
+        rewind(tmp_file);
+
+        // Read from the pipe and write to the temporary file
+        char buffer[1024];
+        ssize_t bytesRead;
+
+        while ((bytesRead = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) {
+            fwrite(buffer, 1, bytesRead, tmp_file);
+        }
+
+        close(pipe_fd[0]);  // Close the read end of the pipe
     }
 
+    // Rewind the temporary file pointer to the beginning
     rewind(tmp_file);
 
     pid = fork();
 
     if (pid == 0) {
-        execlp("tar", "tar", "xf","-f", "-", NULL);
+        // Child process: Execute tar command to extract the archive
+        execlp("tar", "tar", "xf", "-", "-f", "." , NULL);
         perror("tar");
         exit(1);
     } else {
+        // Parent process: Wait for the child process to finish
         wait(NULL);
     }
 
@@ -61,30 +93,25 @@ int myunzip(char *file) {
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        printf("Usage: %s <file>\n", argv[0]);
+        printf("Usage: %s <compressed_file>\n", argv[0]);
         exit(1);
     }
 
-    char *filename = argv[1];
+    char *compressedFile = argv[1];
 
     // Validate file
-    if (access(filename, F_OK) != 0) {
-        printf("Error: File %s does not exist\n", filename);
-        exit(1);
-    }
-
-    if (access(filename, R_OK) != 0) {
-        printf("Error: No read permissions for %s\n", filename);
+    if (access(compressedFile, F_OK) != 0) {
+        printf("Error: File %s does not exist\n", compressedFile);
         exit(1);
     }
 
     // Call myunzip
-    int status = myunzip(filename);
+    int status = myunzip(compressedFile);
 
     if (status == 0) {
-        printf("Archive extracted successfully\n");
+        printf("File successfully uncompressed\n");
     } else {
-        printf("Error extracting archive\n");
+        printf("Error uncompressing file\n");
     }
 
     return 0;
